f9a4017dacbdddcfbdea91610529c119fbc2bfd2
donggyun-han <hdg5639@daum.net>
2026-06-17 17:37:04 +0900

    refactor(swagger): SecurityConfig에 Swagger 엔드포인트 추가, oauth2 의존성 제거 및 jwt 의존성 교체

--- Backend/Auth/src/main/java/gj/cloud/auth/global/config/SecurityConfig.java
+++ Backend/Auth/src/main/java/gj/cloud/auth/global/config/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig {
                                 "/actuator/health",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/webjars/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
