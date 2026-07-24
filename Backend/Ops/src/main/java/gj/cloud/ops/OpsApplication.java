78886d64437b11f1253186747c63fbb67c51c101
한동균 <107734276+hdg5639@users.noreply.github.com>
2026-07-24 14:37:50 +0900

    refactor(Ops): Application에 Seoul TimeZone 추가

--- Backend/Ops/src/main/java/gj/cloud/ops/OpsApplication.java
+++ Backend/Ops/src/main/java/gj/cloud/ops/OpsApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class OpsApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.setProperty("user.timezone", "Asia/Seoul");
         SpringApplication.run(OpsApplication.class, args);
     }
 
